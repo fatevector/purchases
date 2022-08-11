c734f5f12ee690ba8e96d776e2f626864b0a740b
Ivan Petrov <fatevector@yandex.ru>
2022-08-11 11:09:18 +0300

    remove tags from counter.jsx

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
-    const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
     const formatCount = () => {
         return count === 0 ? 'empty' : count;
     };
@@ -18,24 +17,7 @@ const Counter = () => {
     const handleDecrement = () => {
         setCount((prevState) => prevState - 1);
     };
-    const handleTagChange = (id) => {
-        setTags(prevState => prevState.filter(tag => tag !== id));
-    };
-    const renderTags = () => {
-        if (tags.length === 0) {
-            return 'No tags';
-        }
-        return tags.map(tag =>
-            <li
-                key={tag}
-                className='btn btn-primary btn-sm m-2'
-                onClick={() => handleTagChange(tag)}
-            >
-                {tag}
-            </li>
-        )
-    };
-    if (tags.length !== 0) return renderTags();
+
     return <>
         <span className={getBadgeClasses()}>{formatCount()}</span>
         <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
