6e1fc74decb5e98dfa9f96baf728478593c26d40
Ivan Petrov <fatevector@yandex.ru>
2022-08-11 13:50:27 +0300

    passing component contentand data to component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 
-const Counter = () => {
-    const [count, setCount] = useState(0);
-    const formatCount = () => {
-        return count === 0 ? "empty" : count;
+const Counter = (props) => {
+    const [value, setValue] = useState(props.value);
+    const formatValue = () => {
+        return value === 0 ? "empty" : value;
     };
     const getBadgeClasses = () => {
         let classes = "badge m-2 ";
-        classes += count === 0 ? "bg-warning " : "bg-primary ";
+        classes += value === 0 ? "bg-warning " : "bg-primary ";
         return classes;
     };
 
     const handleIncrement = () => {
-        setCount((prevState) => prevState + 1);
+        setValue((prevState) => prevState + 1);
     };
     const handleDecrement = () => {
-        setCount((prevState) => prevState - 1);
+        setValue((prevState) => prevState - 1);
     };
-
     return (
         <div>
-            <span className={getBadgeClasses()}>{formatCount()}</span>
+            <span>{props.name}</span>
+            <span className={getBadgeClasses()}>{formatValue()}</span>
             <button
                 className="btn btn-primary btn-sm m-2"
                 onClick={handleIncrement}
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -3,14 +3,20 @@ import Counter from "./counter";
 
 const CountersList = () => {
     const counters = [
-        { id: 0, value: 0 },
-        { id: 1, value: 4 },
-        { id: 2, value: 0 },
+        { id: 0, value: 0, name: "Ненужная вещь" },
+        { id: 1, value: 4, name: "Ложка" },
+        { id: 2, value: 0, name: "Вилка" },
+        { id: 3, value: 0, name: "Тарелка" },
+        { id: 4, value: 0, name: "Набор минималиста" },
     ];
     return (
         <>
             {counters.map((counter) => (
-                <Counter key={counter.id} />
+                <Counter
+                    key={counter.id}
+                    value={counter.value}
+                    name={counter.name}
+                />
             ))}
         </>
     );
