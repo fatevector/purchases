e8e56329ebc0d3251759e2d4f1d7f7de29f2604a
Ivan Petrov <fatevector@yandex.ru>
2022-08-14 23:30:12 +0300

    add single source of state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
-
 const Counter = (props) => {
-    const [value, setValue] = useState(props.value);
+    const { id, value, name, onIncrement, onDecrement, onDelete } = props;
     const formatValue = () => {
         return value === 0 ? "empty" : value;
     };
@@ -11,31 +9,25 @@ const Counter = (props) => {
         return classes;
     };
 
-    const handleIncrement = () => {
-        setValue((prevState) => prevState + 1);
-    };
-    const handleDecrement = () => {
-        setValue((prevState) => prevState - 1);
-    };
     return (
         <div>
-            <span>{props.name}</span>
+            <span>{name}</span>
             <span className={getBadgeClasses()}>{formatValue()}</span>
             <button
                 className="btn btn-primary btn-sm m-2"
-                onClick={handleIncrement}
+                onClick={() => onIncrement(id)}
             >
                 +
             </button>
             <button
                 className="btn btn-primary btn-sm m-2"
-                onClick={handleDecrement}
+                onClick={() => onDecrement(id)}
             >
                 -
             </button>
             <button
                 className="btn btn-danger btn-sm m-2"
-                onClick={() => props.onDelete(props.id)}
+                onClick={() => onDelete(id)}
             >
                 Delete
             </button>
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -2,27 +2,71 @@ import { useState } from "react";
 import Counter from "./counter";
 
 const CountersList = () => {
-    const [counters, setCounters] = useState([
+    const initialState = [
         { id: 0, value: 0, name: "Ненужная вещь" },
         { id: 1, value: 4, name: "Ложка" },
         { id: 2, value: 0, name: "Вилка" },
         { id: 3, value: 0, name: "Тарелка" },
         { id: 4, value: 0, name: "Набор минималиста" },
-    ]);
+    ];
+    const [counters, setCounters] = useState(initialState);
     const handleDelete = (id) => {
         setCounters(counters.filter((counter) => counter.id !== id));
+    };
+    const handleReset = () => {
+        setCounters(initialState);
+    };
+    const handleUpdate = () => {
+        const updatedState = [
+            { id: 0, value: 1, name: "Ненужная вещь" },
+            { id: 1, value: 2, name: "Ложка" },
+            { id: 2, value: 3, name: "Вилка" },
+            { id: 3, value: 4, name: "Тарелка" },
+            { id: 4, value: 0, name: "Набор минималиста" },
+        ];
+        setCounters(updatedState);
+    };
+    const handleIncrement = (id) => {
+        setCounters((prevState) => {
+            return prevState.map((counter) =>
+                counter.id === id
+                    ? { ...counter, value: counter.value + 1 }
+                    : counter
+            );
+        });
+    };
+    const handleDecrement = (id) => {
+        setCounters((prevState) => {
+            return prevState.map((counter) =>
+                counter.id === id
+                    ? { ...counter, value: counter.value - 1 }
+                    : counter
+            );
+        });
     };
     return (
         <>
             {counters.map((counter) => (
                 <Counter
                     key={counter.id}
-                    id={counter.id}
-                    value={counter.value}
-                    name={counter.name}
+                    {...counter}
                     onDelete={handleDelete}
+                    onIncrement={handleIncrement}
+                    onDecrement={handleDecrement}
                 />
             ))}
+            <button
+                className="btn btn-primary btn-sm m-2"
+                onClick={handleReset}
+            >
+                Сброс
+            </button>
+            <button
+                className="btn btn-primary btn-sm m-2"
+                onClick={handleUpdate}
+            >
+                Обновить состояние
+            </button>
         </>
     );
 };
