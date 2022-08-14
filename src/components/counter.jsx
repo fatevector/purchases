27f602bbb4563ea552a4cc868bae0efff7b98029
Ivan Petrov <fatevector@yandex.ru>
2022-08-14 16:20:51 +0300

    creating and handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -33,6 +33,12 @@ const Counter = (props) => {
             >
                 -
             </button>
+            <button
+                className="btn btn-danger btn-sm m-2"
+                onClick={props.onDelete}
+            >
+                Delete
+            </button>
         </div>
     );
 };
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import { useState } from "react";
 import Counter from "./counter";
 
 const CountersList = () => {
-    const counters = [
+    const [counters, setCounters] = useState([
         { id: 0, value: 0, name: "Ненужная вещь" },
         { id: 1, value: 4, name: "Ложка" },
         { id: 2, value: 0, name: "Вилка" },
         { id: 3, value: 0, name: "Тарелка" },
         { id: 4, value: 0, name: "Набор минималиста" },
-    ];
+    ]);
+    const handleDelete = () => {
+        console.log("handleDelete");
+    };
     return (
         <>
             {counters.map((counter) => (
@@ -16,6 +19,7 @@ const CountersList = () => {
                     key={counter.id}
                     value={counter.value}
                     name={counter.name}
+                    onDelete={handleDelete}
                 />
             ))}
         </>
