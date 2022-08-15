ac9576e8e538173c097607ca70b63f66679778a5
Ivan Petrov <fatevector@yandex.ru>
2022-08-15 16:09:40 +0300

    delete update button

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -16,16 +16,6 @@ const CountersList = () => {
     const handleReset = () => {
         setCounters(initialState);
     };
-    const handleUpdate = () => {
-        const updatedState = [
-            { id: 0, value: 1, name: "Ненужная вещь" },
-            { id: 1, value: 2, name: "Ложка" },
-            { id: 2, value: 3, name: "Вилка" },
-            { id: 3, value: 4, name: "Тарелка" },
-            { id: 4, value: 0, name: "Набор минималиста" },
-        ];
-        setCounters(updatedState);
-    };
     const handleIncrement = (id) => {
         setCounters((prevState) => {
             return prevState.map((counter) =>
@@ -60,12 +50,6 @@ const CountersList = () => {
                 onClick={handleReset}
             >
                 Сброс
-            </button>
-            <button
-                className="btn btn-primary btn-sm m-2"
-                onClick={handleUpdate}
-            >
-                Обновить состояние
             </button>
         </>
     );
